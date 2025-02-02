3eeb290add74e885e875f974b6f1d44aed346f50
TamarEngel <tamar05861@gmail.com>
2025-02-03 01:31:14 +0200

    thank

---
+++ ToDoListReact-master/src/service.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+axios.defaults.baseURL = "http://localhost:5218";
+
+// הוספת interceptor לשגיאות
+axios.interceptors.response.use(
+  response => response, 
+  error => {
+    console.error('API Error:', error.response ? error.response.data : error.message);
+    return Promise.reject(error); 
+  }
+);
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`/items`)    
+    return result.data;
+  },
+
+  addTask: async(name)=>{
+    const result = await axios.post(`/items`,{
+      name:name,
+      isComplete:false
+    })
+    return result.data;
+  },
+
+  setCompleted: async(id,isComplete)=>{
+    const result = await axios.put(`/items/${id}?iscomplete=${isComplete}`, {});
+    return result.data;
+  },
+
+  deleteTask:async(id)=>{
+    const result = await axios.delete(`/items/${id}`,id)
+    return result.data;
+  }
+};
