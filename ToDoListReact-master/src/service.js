ac769fe9e109bfdd25a93af16042e870743cdbde
TamarEngel <tamar05861@gmail.com>
2025-02-03 20:34:12 +0200

    globalVariable

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:5218";
+//axios.defaults.baseURL = "http://localhost:5218";
+axios.defaults.baseURL =process.env.REACT_APP_API_URL;
 
 // הוספת interceptor לשגיאות
 axios.interceptors.response.use(
